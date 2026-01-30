abb19f1bf54ac3e694419982469d7d5926f462be
jiwon <jiwon@gmail.com>
2026-01-30 16:51:26 +0900

    fix: supabase client error in production

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
-import { middleware as updateSession } from '@/lib/supabase/middleware'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  await supabase.auth.getSession();
+  return res;
 }
 
 export const config = {
